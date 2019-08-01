414ee4ef06821de40c91ff879a7b89c5523c60e8
Vaishak Kallore <vaikoovery@gmail.com>
2019-08-01 16:20:45 +0530

    Dummy login with promise

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import './App.css'
 import {
   LabelOutsideCheckBox,
@@ -11,6 +11,9 @@ function App() {
   return (
     <div className="App">
       <Router>
+        <div style={{ marginBottom: '1rem' }}>
+          <Link to="/">Home</Link> <Link to="/login">Login</Link>
+        </div>
         <Switch>
           <Route exact={true} path="/">
             <LabelOutsideCheckBox message="Testing Outside" />
--- src/Login.js
+++ src/Login.js
@@ -1,14 +1,65 @@
 import React, { useReducer } from 'react'
 
-const handleSubmit = () => {}
-
 const Login = () => {
+  const handleSubmit = e => {
+    e.preventDefault()
+    dispatch({ type: 'disable' })
+    dispatch({ type: 'set_message', message: null })
+
+    // Random form submit success status
+    const dividableByThree = Math.floor(Math.random() * 1000) % 3
+
+    const LoginResponse = new Promise((resolve, reject) => {
+      /**
+       * Resolves true if the random number divisible by three.
+       */
+      setTimeout(() => {
+        const canResolve = dividableByThree === 0
+        if (canResolve) resolve(canResolve)
+        reject('Failed!')
+      }, 200)
+    })
+    LoginResponse.then(data => {
+      dispatch({ type: 'enable' })
+    }).catch(e => {
+      dispatch({ type: 'enable' })
+      const message = e.toString()
+      dispatch({ type: 'set_message', message })
+    })
+  }
+
+  const [state, dispatch] = useReducer(
+    (state, action) => {
+      switch (action.type) {
+        case 'disable':
+          return { ...state, processing: true }
+        case 'enable':
+          return { ...state, processing: false }
+        case 'set_message':
+          return { ...state, message: action.message }
+        default:
+          return state
+      }
+    },
+    {
+      processing: false,
+      message: null
+    }
+  )
+  const { processing, message } = state
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <div className="form-group">
           <label>Username:</label>
-          <input type="email" name="username" placeholder="Username" />
+          <input
+            disabled={processing}
+            type="email"
+            name="username"
+            placeholder="Enter your username"
+          />
+          {message === null ? null : <br />}
+          {message}
         </div>
       </form>
     </div>
